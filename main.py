d3d32d58b8f4acab221d16f8eb5b0bbc7e6b3af2
AlexandrKudrya <70281496+AlexandrKudrya@users.noreply.github.com>
2021-04-19 20:46:39 +0900

    Cheking

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import random
 
+import flask.views
 from flask import Flask, render_template, redirect, make_response, request, sessions, url_for
 from flask_login import login_user, current_user, LoginManager
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, BooleanField, SubmitField, StringField, IntegerField, FieldList
+from wtforms import PasswordField, BooleanField, SubmitField, StringField, IntegerField, FieldList, FormField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired
 import datetime
 from data import db_session
 from data import users, question, game
+import json
+from collections import namedtuple
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -98,7 +101,9 @@ def create_game():
         ngame.questions_id = ";".join(arr[:int(form.num_questions.data)])
         ngame.key = ''.join(str(random.randint(1, 150)) for i in range(10))
         db_sess.add(ngame)
-
+        game_data = {'data': {}, 'points': {}}
+        with open(f'game_logs/{ngame.key}.json', 'w') as cat_file:
+            json.dump(game_data, cat_file)
         db_sess.commit()
         res = make_response(render_template("start_game.html", title="Главная Страница", key=ngame.key))
         res.set_cookie("game_key", ngame.key,
@@ -143,7 +148,17 @@ def newgame():
             res.set_cookie("pos", str(pos + 1),
                            max_age=60 * 60 * 24)
             if form.answer.data.lower() == qust.answer.lower():
-                res.set_cookie('score', str(int(request.cookies.get("score", 0))), max_age=60 * 60 * 24)
+                res.set_cookie('score', str(int(request.cookies.get("score", 0)) + 1), max_age=60 * 60 * 24)
+            with open(f'game_logs/{current_game.key}.json', 'r') as cat_file:
+                data = json.load(cat_file)
+            data['data'][str(current_user.id)] = data['data'].get(str(current_user.id), []) + [[qust.answer, form.answer.data, qust.question]]
+            try:
+                k = data['points'][str(current_user.id)]
+            except KeyError:
+                data['points'][str(current_user.id)] = 0
+            print(data)
+            with open(f'game_logs/{current_game.key}.json', 'w') as cat_file:
+                json.dump(data, cat_file, ensure_ascii=False)
             return res
         except IndexError:
             return redirect('/')
@@ -165,6 +180,58 @@ def newgame():
             return res
     else:
         return redirect("/create_game")
+
+
+@app.route('/check_my_games', methods=['GET', 'POST'])
+def check_my_games():
+    current_games = list(db_sess.query(game.Game).filter(game.Game.admin == current_user.id))
+    return render_template('check_my_games.html', arr=current_games)
+
+
+@app.route('/check_my_game/<gameid>', methods=['GET', 'POST'])
+def check_my_game(gameid):
+    db_sess = db_session.create_session()
+    cgame = db_sess.query(game.Game).filter(game.Game.id == int(gameid)).first()
+    with open(f'game_logs/{cgame.key}.json', 'r') as cat_file:
+        data = json.load(cat_file)
+    arr_of_answ = data['data']
+    arr_of_normal_answers = [
+        db_sess.query(question.Question).filter(question.Question.id == id).first().answer
+        for id in cgame.questions_id.split(';')
+    ]
+    product = namedtuple('Product', ['yes'])
+
+    fdata = {
+        'save': 'Widgets',
+        'products': [
+            product(data['points'][str(i)])
+            for i in list(arr_of_answ.keys())
+        ]
+    }
+    form = SaveForm(data=fdata)
+    if str(request).split()[-1][:-1] == '[POST]':
+        for i in range(len(list(arr_of_answ.keys()))):
+            user = db_sess.query(users.User).filter(users.User.id == int(list(arr_of_answ.keys())[i])).first()
+            print(user)
+            user.points += int(form.products[i].yes.data) - data['points'][str(list(arr_of_answ.keys())[i])]
+            print(data)
+            data['points'][str(user.id)] = int(form.products[i].yes.data)
+        db_sess.commit()
+        with open(f'game_logs/{cgame.key}.json', 'w') as cat_file:
+            json.dump(data, cat_file, ensure_ascii=False)
+        return render_template('check_my_game.html', arr_of_answ=arr_of_answ,
+                                   arr_of_normal_answers=arr_of_normal_answers,
+                                   users_id=arr_of_answ.keys(),
+                                   form=form)
+    return render_template('check_my_game.html', arr_of_answ=arr_of_answ,
+                                                 arr_of_normal_answers=arr_of_normal_answers,
+                                                 users_id=arr_of_answ.keys(),
+                                                 form=form)
+
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    return 'я сделаль, потом доделаю'
 
 
 class LoginForm(FlaskForm):
@@ -204,6 +271,15 @@ class AnswerForm(FlaskForm):
 class ChoiceForm(FlaskForm):
     key = StringField('Ключ Игры', validators=[DataRequired()])
     create = SubmitField('Подтвердить')
+
+
+class Cheking_Form(FlaskForm):
+    yes = IntegerField('Счёт')
+
+
+class SaveForm(FlaskForm):
+    products = FieldList(FormField(Cheking_Form), min_entries=1, max_entries=9999)
+    save = SubmitField('Сохранить')
 
 
 if __name__ == '__main__':
